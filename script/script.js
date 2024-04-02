c4a446aeea18b7fcc01cca1e1f1dc667b453df51
GabrielSaltarelli <162124809+GabrielSaltarelli@users.noreply.github.com>
2024-04-02 13:50:48 +0200

    Update script.js

--- script/script.js
+++ script/script.js
@@ -1,6 +1,6 @@
 function doLogin(allert) {
-    var usern = document.getElementById("Username") .value;
-    var passw = document.getElementById("Password") .value;
+    var usern = document.getElementById("exampleInputUsername") .value;
+    var passw = document.getElementById("exampleInputPassword") .value;
       if(usern == "" || passw == "") {
         alert ("Username e password sono obbligatori"); 
     }
