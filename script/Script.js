6b9f8d6dd17cbde93c7a9c12e6a3045dd4872574
GabrielSaltarelli <162124809+GabrielSaltarelli@users.noreply.github.com>
2024-04-02 10:15:33 +0200

    Create Script.js

---
+++ script/Script.js
@@ -0,0 +1,7 @@
+function doLogin () {
+  document.getElement.Byld ("username") .value;
+  document. getElementById ("password") .value;
+    if (document.getElementById ("username"). value == "" || document.getElementById ("password"). value == "") {
+alert ("Username e password sono obbligatori"); 
+    }
+  }
