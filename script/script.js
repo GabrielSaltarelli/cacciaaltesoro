1ad132ad1b76452290b6b1a4b9d47ba5d413ac50
GabrielSaltarelli <162124809+GabrielSaltarelli@users.noreply.github.com>
2024-04-02 11:30:21 +0200

    Update script.js

--- script/script.js
+++ script/script.js
@@ -1,2 +1,7 @@
-
-console.log() ;
+function doLogin(allert) {
+    var usern = document.getElementById ("username") .value;
+    var passw = document.getElementById ("password") .value;
+      if(usern == "" || passw == "") {
+        alert ("Username e password sono obbligatori"); 
+    }
+  }
