19a3a8970d1b7257bf8beeb9c547a8baef1a7624
GabrielSaltarelli <162124809+GabrielSaltarelli@users.noreply.github.com>
2024-04-08 21:57:06 +0200

    Update script.js

--- script/script.js
+++ script/script.js
@@ -5,3 +5,21 @@ function doLogin(allert) {
         alert ("Username e password sono obbligatori"); 
     }
   }
+      else {
+          fetch('https://www.cacciaapi.altervista.org/login.php/loginIpad', {
+              method: 'POST',
+              headers: {
+                  'Content-Type': 'application/json',
+              },
+              body: JSON.stringify({
+                  "username": username,
+                  "password": password
+              })
+          })
+          .then(response => responsive.json())
+          .then(data => console.log(data))
+          .catch((error) =>{
+              console.error('Error:', error);
+          });
+
+      }
