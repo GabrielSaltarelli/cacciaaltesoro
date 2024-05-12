c29c5a27b8314f67ee6344c65a0c977fdaced8b8
GabrielSaltarelli <162124809+GabrielSaltarelli@users.noreply.github.com>
2024-05-12 20:08:15 +0200

    Update and rename scriptoggetto3 to scriptoggetto3.js

--- script/scriptoggetto3.js
+++ script/scriptoggetto3.js
@@ -6,10 +6,10 @@ else {
               },
               body: JSON.stringify({
                  [
-                  { "Film": "Fear", "Visto": "si", "Testo": "Fear", "Valutazione": "3/5" }, //0
-                  { "Film": "One Life", "Visto": "no", "Testo": "One Life", "Valutazione": "3.6/5" }, //1
-                  { "Film": "L'ordine del tempo", "no": "Attivo", "Testo": "L'origine del tempo" "Valutazione": "2.7/5" }, //2
-                  { "Film": "Tutti tranne te, "Visto": "si", "Testo": "Tutti tranne te" "Valutazione": "2.5/5" }, //3
+                  { "Testo": "Fear", "Valutazione": "3/5", "Visto?": "si" }, //0
+                  { "Testo": "One Life", "Valutazione": "3.6/5",  "Visto?": "si" }, //1
+                  { "Testo": "L'origine del tempo" "Valutazione": "2.7/5",  "Visto?": si" }, //2
+                  { "Testo": "Tutti tranne te" "Valutazione": "2.5/5",  "Visto?": "si" }, //3
                   ]
               })
           })
